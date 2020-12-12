a9e0b22e6b50c74b4d3d3bce3d6ddda07852703b
Lovisa Hassler <lovisa.hassler@gmail.com>
2020-12-12 13:17:52 +0100

    Improved text effect circular image

--- src/components/CircularImage.tsx
+++ src/components/CircularImage.tsx
@@ -11,25 +11,40 @@ interface Props{
 interface MaskProps extends HTMLAttributes<HTMLDivElement>{
   size: number;
   factor: number;
+}
+
+interface ImgProps extends HTMLAttributes<HTMLDivElement>{
   url: string;
   blur: string;
 }
 
 const Mask = styled.div<MaskProps>`
+  postion: relative;
   display: flex;
   height: 200px;
   width: 200px;
   clip-path: ${(p) => `circle(${p.size}px at center)`};
-  filter: ${(p) => `blur(${p.blur})`};
-  transition: 0.1s;
+  transition: 0.3s;
   text-align: center;
   justify-content: center;
   align-items: center;
-  background: ${(p) => `url(${p.url})`};
-  text-color: red;
+  color: rgba(255, 255, 255, 0.8);
 
   &:hover{
     clip-path:  ${(p) => `circle(${p.size * p.factor }px at center)`};
+    color: rgba(255, 255, 255, 0);
+  }
+`;
+
+const Img = styled.div<ImgProps>`
+  position: absolute;
+  height: 200px;
+  width: 200px;
+  filter: ${(p) => `blur(${p.blur})`};
+  transition: 0.2s;
+  background: ${(p) => `url(${p.url})`};
+
+  &:hover{
     filter: blur(0);
   }
 `;
@@ -37,8 +52,9 @@ const Mask = styled.div<MaskProps>`
 const CircularImage: FC<Props> = ({imgSrc, text = '', diameter = 60, hoverEffect = true}) => {
   return (
     <div>
-      <Mask size={diameter} blur={hoverEffect ? '4px' : '0'} factor={hoverEffect ? 1.1 : 1} url={imgSrc} >
-        <div className='text-button'>{text}</div>
+      <Mask size={diameter} factor={hoverEffect ? 1.1 : 1} >
+        <Img url={imgSrc} blur={hoverEffect ? '4px' : '0'} />
+        <div className='circular-image-text'>{text}</div>
       </Mask>
     </div>
   );
